15acda8ce43a9c63bf12f7a41ecfcd5b7d0be239
Vadim Savin <savinvadim1312@gmail.com>
2024-02-16 18:43:23 +0000

    Users CRUD Mongo

--- src/graphql/models/user.js
+++ src/graphql/models/user.js
@@ -1,47 +1,68 @@
+import { ObjectId } from 'mongodb';
+
 export const typeDef = /* GraphQL */ `
   type Query {
-    user: User
+    users: [User!]!
+    user(id: ID!): User
   }
 
   type Mutation {
     createUser(user: NewUserInput!): User
+    deleteUser(id: ID!): Boolean
+    updateUser(id: ID!, update: UpdateUserInput): User
   }
 
   input NewUserInput {
     name: String!
-    age: Int!
+    email: String!
+  }
+
+  input UpdateUserInput {
+    name: String!
   }
 
   type User {
-    id: Int
+    id: ID!
     name: String
-    age: Int
+    email: String
   }
 `;
 
 export const resolvers = {
   Query: {
-    user: () => {
-      return {
-        id: 1,
-        name: 'Vadim',
-      };
+    users: (obj, args, { mongo }) => {
+      return mongo.users.find().limit(20).toArray();
+    },
+
+    user: async (obj, { id }, { mongo }) => {
+      return mongo.users.findOne({ _id: new ObjectId(id) });
     },
   },
 
   Mutation: {
     createUser: async (_, { user }, { mongo }) => {
-      const movies = await mongo.movies.find().toArray();
-      console.log(movies);
-      // insert into DB
+      const response = await mongo.users.insertOne(user);
       return {
-        id: 1,
+        id: response.insertedId,
         ...user,
       };
+    },
+    deleteUser: async (obj, { id }, { mongo }) => {
+      await mongo.users.deleteOne({ _id: new ObjectId(id) });
+      return true;
+    },
+    updateUser: async (obj, { id, update }, { mongo }) => {
+      console.log(update);
+      const response = await mongo.users.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { name: update.name } }
+      );
+      return mongo.users.findOne({ _id: new ObjectId(id) });
     },
   },
 
   User: {
+    id: ({ id, _id }) => _id || id,
     name: (obj) => {
       return obj.name.trim().toUpperCase();
     },
